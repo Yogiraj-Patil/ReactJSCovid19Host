e670af28b600b6c89fba39631a5da181213a1eec
Yogiraj-Patil <yogiraj80555@gmail.com>
2020-10-25 20:52:21 +0530

    Updates

---
+++ precache-manifest.b3bf51dc44e305b1b7e5a4460492bf1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6802b4d4e2d1dce04f6e701066bcfc45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe1cf9f422d60ef33fc4",
+    "url": "/static/css/main.d99094cf.chunk.css"
+  },
+  {
+    "revision": "8ed07b4a615502ebe212",
+    "url": "/static/js/2.32cc54c1.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/static/js/2.32cc54c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe1cf9f422d60ef33fc4",
+    "url": "/static/js/main.c63eeb8f.chunk.js"
+  },
+  {
+    "revision": "a778b24497f0903ec703",
+    "url": "/static/js/runtime-main.90f2d14d.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
